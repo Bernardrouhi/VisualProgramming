6decd91b42cd7e88bf1a031722f87c86dcc698b9
Bernard Rouhi <bernardrouhi@gmail.com>
2025-12-23 14:50:49 +0000

    Updated the PkgResources
    Added load and read the stylesheet

--- Source/VisualProgramming/Core/PkgResource.py
+++ Source/VisualProgramming/Core/PkgResource.py
@@ -1,1 +1,15 @@
-class PkgResource: ...
+from pathlib import Path
+from PySide6.QtCore import *
+
+
+class PkgResource:
+    RESOURCES_DIR = str(Path(__file__).parent.parent / "Resources" / "Styles")
+
+    @classmethod
+    def stylesheet(cls, fileName: str) -> str:
+        if not fileName.lower().endswith(".qss"):
+            fileName += ".qss"
+        path = Path(cls.RESOURCES_DIR) / fileName
+        file = QFile(path)
+        file.open(QFile.OpenModeFlag.ReadOnly)
+        return file.readAll().toStdString()
